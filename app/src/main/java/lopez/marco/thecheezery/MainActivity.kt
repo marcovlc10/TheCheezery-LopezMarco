901f2a57e215acea242ea052b9e753b55b59a060
marcovlc10 <marcos.mvlpcz@gmail.com>
2023-02-09 14:46:29 -0700

    commit registry activity

---
+++ app/src/main/java/lopez/marco/thecheezery/MainActivity.kt
@@ -0,0 +1,11 @@
+package lopez.marco.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
