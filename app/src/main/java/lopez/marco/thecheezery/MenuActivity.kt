9e10da79d140446efb4c9a35d8d7d31060d301c8
marcovlc10 <marcos.mvlpcz@gmail.com>
2023-03-11 12:53:34 -0700

    productos agregados

--- app/src/main/java/lopez/marco/thecheezery/MenuActivity.kt
+++ app/src/main/java/lopez/marco/thecheezery/MenuActivity.kt
@@ -1,11 +1,39 @@
 package lopez.marco.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks)
+        var btnHot: Button = findViewById(R.id.button_hot_drinks)
+        var btnSweets: Button = findViewById(R.id.button_sweets)
+        var btnSalties: Button = findViewById(R.id.button_salties)
+
+        btnCold.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "1")
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "2")
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "3")
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java).putExtra("typo", "4")
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/lopez/marco/thecheezery/Product.kt
@@ -0,0 +1,6 @@
+package lopez.marco.thecheezery;
+
+data class Product (var name:String,
+                    var image:Int,
+                    var description:String,
+                    var price:Double)
